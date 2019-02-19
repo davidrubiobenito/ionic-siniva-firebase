98db7f8dbdb47becc95d70771e0c16b5ed2b8113
David <davidrubiobenito@gmail.com>
2019-02-19 21:00:43 +0100

    .gitignore is now working

--- src/app/firebase.credentials.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyCobDHrJpyqaf6jdPIzfOa0sRnBi802WZk",
-    authDomain: "ionic-crud-v1.firebaseapp.com",
-    databaseURL: "https://ionic-crud-v1.firebaseio.com",
-    projectId: "ionic-crud-v1",
-    storageBucket: "ionic-crud-v1.appspot.com",
-    messagingSenderId: "1073055849210"
-};
